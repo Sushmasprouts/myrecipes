38816f05c550c29543aeb189730704d51b3aa8f5
sushma <sushma@sproutssolutions.com>
2017-10-30 10:28:22 +0000

    refactor and clean layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only:[:show,:edit,:update]
   
   def index
   @recipes = Recipe.all
@@ -9,7 +10,7 @@ class RecipesController < ApplicationController
   end
   
   def show
-    @recipe= Recipe.find(params[:id])
+  
   end
   
   def create
@@ -24,12 +25,11 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
-    
+   
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
+   
     if @recipe.update(recipe_params)
       flash[:success] = "Recipe was updated successfully!"
      redirect_to recipe_path(@recipe)
@@ -46,6 +46,10 @@ class RecipesController < ApplicationController
   end
 
   private
+  
+   def set_recipe
+      @recipe = Recipe.find(params[:id])
+   end
    def recipe_params
      params.require(:recipe).permit(:name,:description)
    end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -29,7 +29,8 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @chef.chefname, response.body
     assert_select "a[href=?]" , edit_recipe_path(@recipe), text: "edit this recipe"
     assert_select "a[href=?]", recipe_path(@recipe), text: "Delete"
-  end
+    assert_select "a[href=?]", recipe_path, text: "return to recipes listing"
+    end
   
   test "to create valid recipes" do 
     get new_recipe_path
