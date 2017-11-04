6439b11d4c49b82a52df0f36b617a15f2654f227
sushma <sushma@sproutssolutions.com>
2017-11-04 07:58:41 +0000

    add chefs destroy

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -37,6 +37,13 @@ class ChefsController < ApplicationController
     end
   end
   
+  def destroy
+    @chef= Chef.find(params[:id])
+    @chef.destroy
+    flash[:destroy]="sucessfuly deleted"
+    redirect_to chefs_path
+  end
+  
   private
   
   def chef_params
--- test/integration/chefs_listing_test.rb
+++ test/integration/chefs_listing_test.rb
@@ -8,9 +8,19 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
    end
   
    test "should get chefs listing" do
-    get chefs_path
-    assert_template 'chefs/index'
-    assert_select "a[href=?]" , chef_path(@chef), text: @chef.chefname
-    assert_select "a[href=?]" , chef_path(@chef2), text: @chef2.chefname
+     get chefs_path
+     assert_template 'chefs/index'
+     assert_select "a[href=?]" , chef_path(@chef), text: @chef.chefname
+     assert_select "a[href=?]" , chef_path(@chef2), text: @chef2.chefname
+   end
+
+  test "should delete chefs " do
+     get chefs_path
+     assert_template 'chefs/index'
+     assert_difference 'Chef.count' , -1 do
+       delete chef_path(@chef)
+     end
+     assert_redirected_to chefs_path
+     assert_not flash.empty?
   end
 end
