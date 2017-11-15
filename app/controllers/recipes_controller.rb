8f2cd691e47649792c4abe8aa2bcae889f6caa07
sushma <sushma@sproutssolutions.com>
2017-11-15 05:27:11 +0000

    make comments real time

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
   end
   
   def show
-    @comment= Comment.new
+   @comment= Comment.new
    @comments= @recipe.comments.paginate(page: params[:page], per_page: 5)
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
   
   root "pages#home"
-  
   get 'pages/home', to: 'pages#home'
    
   resources :recipes do
     resources :comments, only: [:create]
   end
+  
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
+  
   get '/login', to: 'sessions#new'
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+  
   resources :incredients, except:[:destroy]
+  
+  mount ActionCable.server => '/cable'
   
 end
